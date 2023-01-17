e9fe9193725501daecc4c803c802c9beda43ab50
kolansurgut <73068398+kolansurgut@users.noreply.github.com>
2023-01-17 15:25:04 +0500

    Update connect.php

--- blocks/connect.php
+++ blocks/connect.php
@@ -1,6 +1,6 @@
 <?php
-    $mysql = new mysqli('37.140.192.240', 'u1844055_carcare',
-    'u1844055_carcare', 'u1844055_carcare', '3306');
+    $mysql = new mysqli('', '',
+    '', '', '');
 
     if (!$mysql)
         echo "нет подключения"
